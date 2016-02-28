0e49cfd8c6ffffb18f67a9d6cadac405b7431cbc
Kong To <newlight77@gmail.com>
2016-02-28 23:53:42 +0100

    Updating customize script

--- customize.sh
+++ customize.sh
@@ -2,23 +2,16 @@
 
 echo "*** Customize *** Customizing User env" 1>&2
 
-cd .macosx
+cd ~
 
-cp .bashrc  ~/.bashrc
-cp .bashrc_natelandau ~/.bashrc_natelandau
-cp .bashrc_alias ~/.bashrc_alias
-cp .bashrc_ps12 ~/.bashrc_ps12
-cp .profile ~/.profile
+git clone https://github.com/magicmonty/bash-git-prompt.git .bash-git-prompt
+git clone https://github.com/google/styleguide.git
+git clone https://github.com/newlight77/macosx.git .macosx
 
 cat .macosx/.bashrc_profile >> ~/.bashrc_profile
 cat .macosx/.gitconfig >> ~/.gitconfig
 
 git config --global user.name "newlight77"
 git config --global user.email "newlight77@gmail.com"
-
-git clone https://github.com/google/styleguide.git
-
-cd ~
-git clone https://github.com/magicmonty/bash-git-prompt.git .bash-git-prompt
 
 ssh-keygen -t rsa
