424ffe3376a22adbf607c7192f4933a09124cbdd
Kong To <newlight77@gmail.com>
2022-05-22 11:07:09 +0200

    remove scripts and bash_custom

--- bash_custom/profile/ssh-copy-id.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "*** ------  copying ssh keys to server ------ ***" 1>&2
-
-#cat ~/.ssh/id_rsa.pub | ssh demo@192.168.1.101 "mkdir -p ~/.ssh && chmod 700 ~/.ssh && cat >>  ~/.ssh/authorized_keys"
-
-# Example
-#ssh-copy-id myserver
